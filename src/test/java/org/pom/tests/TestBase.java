2434cff612413ebe26b34ba6abff34279a4d27e9
Natalia Hrytsenia <hrytsenianatalia@gmail.com>
2025-12-25 19:58:24 +0100

    First commit

---
+++ src/test/java/org/pom/tests/TestBase.java
@@ -0,0 +1,26 @@
+package org.pom.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void init(){
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com");
+        driver.manage().window().maximize(); //разворот на весь экран
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //ожидание загрузки всех элементов
+    }
+
+    @AfterMethod(enabled = false) //временно отключаем метод
+    public void tearDown(){
+        driver.quit();
+    }
+}
